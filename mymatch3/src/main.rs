e50aaf4535db694f6c6fb4bcd46366a3e64aaccb
Emilia Tyl <emilia.tyl@huuugegames.com>
2024-03-02 11:40:45 +0100

    First setup system added, printing a text label

--- mymatch3/src/main.rs
+++ mymatch3/src/main.rs
@@ -2,11 +2,14 @@ use bevy::{
     prelude::*
 };
 
+mod systems;
+use crate::systems::setup_system;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        /*.add_systems(Startup, ())
-        .add_systems(
+        .add_systems(Startup, setup_system::setup)
+        /*.add_systems(
             FixedUpdate,
             ()
                 // `chain`ing systems together runs them in order
---
+++ mymatch3/src/systems/mod.rs
@@ -0,0 +1,49 @@
+pub mod setup_system {
+    use bevy::{
+        math::bounding::{Aabb2d, BoundingCircle, BoundingVolume, IntersectsVolume},
+        prelude::*,
+        ecs::system::Commands,
+        sprite::MaterialMesh2dBundle,
+    };
+
+    #[derive(Component)]
+    struct ScoreboardUi;
+
+    const SCORE_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
+    const SCOREBOARD_FONT_SIZE: f32 = 40.0;
+    const SCOREBOARD_TEXT_PADDING: Val = Val::Px(5.0);
+    const TEXT_COLOR: Color = Color::rgb(0.5, 0.5, 1.0);
+
+    pub fn setup(
+        mut commands: Commands
+    ) {
+        // Camera
+        commands.spawn(Camera2dBundle::default());
+
+        // Scoreboard
+        commands.spawn((
+            ScoreboardUi,
+            TextBundle::from_sections([
+                TextSection::new(
+                    "Score: ",
+                    TextStyle {
+                        font_size: SCOREBOARD_FONT_SIZE,
+                        color: TEXT_COLOR,
+                        ..default()
+                    },
+                ),
+                TextSection::from_style(TextStyle {
+                    font_size: SCOREBOARD_FONT_SIZE,
+                    color: SCORE_COLOR,
+                    ..default()
+                }),
+            ])
+                .with_style(Style {
+                    position_type: PositionType::Absolute,
+                    top: SCOREBOARD_TEXT_PADDING,
+                    left: SCOREBOARD_TEXT_PADDING,
+                    ..default()
+                }),
+        ));
+    }
+}
